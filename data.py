0718cd483a2c4b3b3f173328f23f31aaa65080c9
angel <angelo.magnago@hotmail.com>
2025-04-21 23:10:30 +0200

    Second commit

--- data.py
+++ data.py
@@ -1,1 +1,8 @@
 # Arquivo com constantes
+URBAN_ROUTES_URL = 'https://cnt-03487441-ba0a-4345-b915-b66e9ec0297b.containerhub.tripleten-services.com?lng=pt'
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
